49477f431536515763c131961afe4bb3375acc82
morantg <bernanosg@gmail.com>
2019-02-25 16:01:59 +0100

    New Config class

--- Main/model/DBFactory.php
+++ Main/model/DBFactory.php
@@ -2,7 +2,8 @@
 class DBFactory{
   
   public static function getMysqlConnexionWithPDO(){
-    $db = new PDO('mysql:host=localhost;dbname=p5_blog', 'root', '');
+  	$config = Config::getInstance('config.php');
+	$db = new PDO("mysql:host=" . $config->get('db_host') . ";dbname=" . $config->get('db_name') , $config->get('db_user') , $config->get('db_pass'));
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->exec('set names utf8');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
